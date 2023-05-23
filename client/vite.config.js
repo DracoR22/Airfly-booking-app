3901217d8ee2b4fdf2c1bc8eb8a2b751d6bf9d34
DracoR22 <rdraco039@gmail.com>
2023-05-23 14:21:40 -0500

    update

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,8 +5,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      '/api': 'http://localhost:3000'
-    }
+   
   }
 })
