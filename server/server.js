0ef20ebf0e6142e1f55ac8df03d4fb9abf8933a0
DracoR22 <rdraco039@gmail.com>
2023-05-23 20:32:47 -0500

    no

--- server/server.js
+++ server/server.js
@@ -18,6 +18,7 @@ const jwtSecret = 'grer3qwer3gtfgfhmkhgfcvcbvngfth'
 const allowedOrigins = [
     'https://airfly-booking-app-hd9f.vercel.app/',
    'http://localhost:5173',
+   'https://airfly-booking-app-hd9f-git-main-rdraco039-gmailcom.vercel.app/',
  ];
 
 app.use(express.json())
