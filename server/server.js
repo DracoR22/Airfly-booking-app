8e251c54e78da74ffc413ea4a0748abce8cc8165
DracoR22 <rdraco039@gmail.com>
2023-05-27 11:12:04 -0500

    3

--- server/server.js
+++ server/server.js
@@ -15,10 +15,12 @@ const app = express()
 
 const jwtSecret = 'grer3qwer3gtfgfhmkhgfcvcbvngfth'
 
+app.use(cors({
+   origin: 'http://localhost:5173',
+   credentials: true,}));
 app.use(express.json())
 app.use(cookieParser())
 app.use('/uploads', express.static(__dirname+'/uploads'))
-app.use(cors())
 
 mongoose.connect(process.env.MONGO_URL)
 
